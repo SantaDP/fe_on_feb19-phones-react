9a5b2642485e19e07a0d2e6262285f33c957676d
Aleksandr Pereleev <aleksandr.pereleev@gmail.com>
2019-06-08 11:09:02 +0300

    Make gallery task

--- src/App.js
+++ src/App.js
@@ -28,7 +28,6 @@ class App extends React.Component {
               <Filter />
               <Basket />
             </div>
-
             <div className="col-md-10">
               { this.state.selectedPhone ? (
                 <Viewer
@@ -45,9 +44,13 @@ class App extends React.Component {
                   onPhoneSelected={(phoneId) => {
                     this.setState({
                       selectedPhone: getById(phoneId),
-                    });
+                    }); console.log('getById  ' + getById)
+                    console.log('selectedPhoneyId  ' + this.state.selectedPhone)
+                    console.log('getById  ' + phoneId)
+                    console.log('phones  ' + this.state.phones)
+                    
                   }}
-                />
+                /> 
               ) }
             </div>
           </div>
@@ -57,23 +60,48 @@ class App extends React.Component {
   }
 }
 
-const Viewer = (props) => (
+
+class Viewer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mainImg: props.phone.images[0],
+      description: props.phone.description,
+      name: props.phone.name,
+      images: props.phone.images,
+
+    };
+
+    this.hanbleShowImg = (imgURl) => {
+      this.setState(()=> {
+        return { mainImg: imgURl };
+      });
+    };
+  }
+  render () {
+    return (
   <div>
-    <img className="phone" src={props.phone.images[0]}/>
-    <button onClick={props.onBack}>Back</button>
+    <img className="phone" src={this.state.mainImg}/>
+    <button onClick={this.props.onBack}>Back</button>
     <button>Add to basket</button>
 
-    <h1>{props.phone.name}</h1>
-    <p>{props.phone.description}</p>
+    <h1>{this.state.name}</h1>
+    <p>{this.state.description}</p>
 
     <ul className="phone-thumbs">
-      { props.phone.images.map(imageUrl => (
+      { this.state.images.map(imageUrl => (
         <li>
-          <img src={imageUrl}/>
+          <img 
+          onClick = {()=> {this.hanbleShowImg(imageUrl)}} 
+          src={imageUrl}/>
         </li>
       )) }
     </ul>
   </div>
-);
+
+    );
+  }
+  
+};
 
 export default App;
